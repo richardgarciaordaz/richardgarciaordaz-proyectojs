dc793872a9ff859589e39319d8eda293477ccc84
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2023-09-29 02:44:04 +0000

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+let action = ["se comió", "orinó en", "aplastó", "rompió"];
+let what = ["mi tarea", "las llaves", "el coche"];
+let when = [
+  "antes de la clase",
+  "justo a tiempo",
+  "cuando terminé",
+  "durante mi almuerzo",
+  "mientras rezaba"
+];
